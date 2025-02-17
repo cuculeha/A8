022a7137c35221c3e31fffb78a69b152984986f8
sbabar0 <sbabar0@saddleback.edu>
2025-02-17 15:56:12 -0800

    added personType.cpp

---
+++ personType.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "personType.h"
+
+using namespace std;
+
+
+// Setters definition
+void personType :: setFName (string fN)
+{
+		fName = fN;
+}
+
+void personType :: setLName (string lN)
+{
+		lName = lN;
+}
+
+void personType :: setAddress (string addr )
+{
+		address = addr;
+}
+
+void personType :: setHeight (string ht)
+{
+		height = ht;
+}
+
+void personType :: setDOB (string bdate)
+{
+		DOB = bdate;
+}
+
+void personType :: setGender (string gen)
+{
+		gender = gen;
+}
+
+
+// Getters definition
