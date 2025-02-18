b8126729e0895cf58b1e126d70fa05c306500242
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-02-17 16:23:36 -0800

    updated implementation file

--- personType.cpp
+++ personType.cpp
@@ -37,3 +37,59 @@ void personType :: setGender (string gen)
 
 
 // Getters definition
+void personType :: getFName ( string &fN ) const
+{
+	fN = fName;
+}
+
+void personType :: getFLName ( string &lN ) const
+{
+	lN = lName;
+}
+
+void personType :: getAddress ( string &addr ) const
+{
+	addr = address;
+}
+
+void personType :: getHeight ( string &ht ) const
+{
+	ht = height;
+}
+
+void personType :: getDOB ( string &bdate ) const
+{
+	bdate = DOB;
+}
+
+void personType :: getGender ( string &gen ) const
+{
+	gen = gender;
+}
+
+// print function
+void personType :: print () const
+{
+	cout << "All persons information...\n";
+	cout << "First Name : " << fName << endl;
+	cout << "Last Name  : " << lName << endl;
+	cout << "Address    : " << address << endl;
+	cout << "Height     : " << height << endl;
+	cout << "DOB        : " << DOB << endl;
+	cout << "Gender     : " << gender << endl;
+}
+
+// equals strings
+bool personType :: equalData (const personType &person2) const
+{
+	if (fName == person2.fName && lName == person2.lName && address == person2.address
+		height == person2.height && DOB == person2.DOB && gender == person2.gender)
+		{
+			return true;
+		}
+
+	else
+		{
+			return false;
+		}
+}
